b89b5f59ac964c8da4154664b6afda3d2d8fbf53
Sheron Jude <53206494+sheron184@users.noreply.github.com>
2022-07-27 10:05:15 -0700

    get and set

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Kata {
+    internal class Kata{
+        
+        class student {
+            string name;
+            string dob;
+            public string Name{
+                get { return name; }
+                set { name = value; }
+            }
+            public string Dob{
+                get { return dob; }
+                set { dob = value; }
+            }
+        }
+        static void Main(string[] args){
+            student student = new student();
+            student.Name = "Sheron";
+            student.Dob = "1999-01-07";
+           
+            Console.WriteLine("Name is : "+student.Name+" DOB is : "+student.Dob);
+        }
+    }
+}
